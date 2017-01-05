90b31496e96027114d679808d0512bb83872fd10
David Lindell <lindell@stanford.edu>
2017-01-04 16:13:50 -0800

    updating flags on helper script

--- surround360_render/scripts/run_render.bash
+++ surround360_render/scripts/run_render.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python2 run_all.py --steps_render --data_dir ~/Downloads/sample_dataset --dest_dir ~/Downloads/sample_dataset --rectify_file $PWD/rectify.yml 
+python2 run_all.py --steps_render --data_dir ~/Downloads/sample_dataset --dest_dir ~/Downloads/sample_dataset --rectify_file $PWD/rectify.yml --verbose --enable_top --enable_bottom --save_debug_images
